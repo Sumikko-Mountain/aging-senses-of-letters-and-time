85afebec1f32f62def789f11107e783b0e33d306
YUIKO SUYAMA <Sumikko-Mountain@users.noreply.github.com>
2020-07-31 23:45:59 +0900

    ぜったいやりかたがよくない

--- docs/index.js
+++ docs/index.js
@@ -12,16 +12,24 @@ $(function () {
   $('#windowWidth').text($(window).width());
 
 
-
-  var obj = document.getElementById('sentece-1');
-  var obj2 = document.getElementById('sentece-2');
   var $obj2 = $('#sentece-2');
+
+  var obj1 = document.getElementById('sentece-1');
+  var obj2 = document.getElementById('sentece-2');
   var obj3 = document.getElementById('sentece-3');
   var obj4 = document.getElementById('sentece-4');
   var obj5 = document.getElementById('sentece-5');
   var obj6 = document.getElementById('sentece-6');
-  var emp1 = document.getElementById('empty-1');
+  var obj7 = document.getElementById('sentece-7');
+  var obj8 = document.getElementById('sentece-8');
+  var obj9 = document.getElementById('sentece-9');
+  var obj10 = document.getElementById('sentece-10');
+  var obj11 = document.getElementById('sentece-11');
+  var obj12 = document.getElementById('sentece-12');
+  var obj13 = document.getElementById('sentece-13');
+  var obj14 = document.getElementById('sentece-14');
 
+  var emp1 = document.getElementById('empty-1');
 
 
 
@@ -46,13 +54,50 @@ $(function () {
   function scroll() {
     var clientRect = obj2.getBoundingClientRect();
     var y = clientRect.top;
-
     console.log(y, $obj2.offset().top);
-    if ($(this).scrollTop() >= windowHeight * 6) {
+
+
+    if ($(this).scrollTop() >= windowHeight * 12) {
+      obj14.style.opacity = 0.0;
+      obj13.style.opacity = 1.0;
+      obj12.style.opacity = 0.0;
+      console.log("sentence-6");
+    } else if ($(this).scrollTop() >= windowHeight * 11) {
+      obj13.style.opacity = 0.0;
+      obj12.style.opacity = 1.0;
+      obj11.style.opacity = 0.0;
+      console.log("sentence-6");
+    } else if ($(this).scrollTop() >= windowHeight * 10) {
+      obj12.style.opacity = 0.0;
+      obj11.style.opacity = 1.0;
+      obj10.style.opacity = 0.0;
+      console.log("sentence-6");
+    } else if ($(this).scrollTop() >= windowHeight * 9) {
+      obj11.style.opacity = 0.0;
+      obj10.style.opacity = 1.0;
+      obj9.style.opacity = 0.0;
+      console.log("sentence-6");
+    } else if ($(this).scrollTop() >= windowHeight * 8) {
+      obj10.style.opacity = 0.0;
+      obj9.style.opacity = 1.0;
+      obj8.style.opacity = 0.0;
+      console.log("sentence-6");
+    } else if ($(this).scrollTop() >= windowHeight * 7) {
+      obj9.style.opacity = 0.0;
+      obj8.style.opacity = 1.0;
+      obj7.style.opacity = 0.0;
+      console.log("sentence-6");
+    } else if ($(this).scrollTop() >= windowHeight * 6) {
+      obj8.style.opacity = 0.0;
+      obj7.style.opacity = 1.0;
+      obj6.style.opacity = 0.0;
+      console.log("sentence-6");
+    } else if ($(this).scrollTop() >= windowHeight * 5) {
+      obj7.style.opacity = 0.0;
       obj6.style.opacity = 1.0;
       obj5.style.opacity = 0.0;
       console.log("sentence-6");
-    } else if ($(this).scrollTop() >= windowHeight * 5) {
+    } else if ($(this).scrollTop() >= windowHeight * 4) {
       obj6.style.opacity = 0.0;
       obj5.style.opacity = 1.0;
       obj4.style.opacity = 0.0;
@@ -60,7 +105,7 @@ $(function () {
     } else if ($(this).scrollTop() >= windowHeight * 3) {
       obj5.style.opacity = 0.0;
       obj4.style.opacity = 1.0;
-      /* obj3.style.opacity = 0.0;*/
+      obj3.style.opacity = 0.0;
       console.log("sentence-4");
     } else if ($(this).scrollTop() >= windowHeight * 2) {
       obj4.style.opacity = 0.0;
@@ -70,7 +115,7 @@ $(function () {
     } else if ($(this).scrollTop() >= windowHeight) {
       obj3.style.opacity = 0.0;
       obj2.style.opacity = 1.0;
-      obj.style.opacity = 0.0;
+      obj1.style.opacity = 0.0;
       console.log("sentence-2");
     } else {
       console.log("sentence-1");
@@ -79,7 +124,7 @@ $(function () {
       obj4.style.opacity = 0.0;
       obj3.style.opacity = 0.0;
       obj2.style.opacity = 0.0;
-      obj.style.opacity = 1.0;
+      obj1.style.opacity = 1.0;
     }
   }
 
