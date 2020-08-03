802b384206b909d1549134b07be1391e2620a903
YUIKO SUYAMA <Sumikko-Mountain@users.noreply.github.com>
2020-08-03 20:18:28 +0900

    allSectionDivsをそのまま継ぎ足していくとどうも事故る　なぜだ
    でもこうしていけばできることはわかった

--- docs/index.js
+++ docs/index.js
@@ -24,7 +24,6 @@ $(function () {
 
   var allSectionDivs = 0;
   const divs_section1 = document.querySelectorAll('.section-1');
-  allSectionDivs += divs_section1.length;
   console.log(divs_section1, divs_section1.length, allSectionDivs);
 
   divs_section1.forEach((div, i) => {
@@ -32,12 +31,15 @@ $(function () {
     $(window).scroll(function () {
       var scroll = $(window).scrollTop();
       if (scroll >= windowHeight * i && scroll < windowHeight * (i + 1)) {
+        //真ん中に到達してから次のブロックが来るまで
         // console.log("in if" + i);
         div.style.opacity = 1.0;
       } else if (scroll >= windowHeight * (i - 1) && scroll < windowHeight * i) {
+        //可視範囲に下から入ってきて真ん中に到達するまで
         // console.log("in else if");
         div.style.opacity = 0.1;
       } else {
+        //それ以外では見せない
         // console.log("in else");
         if (i != divs_section1.length - 1) {//最後の一つは消えないようにして、見えてる状態で上に送る iは０から加算さレてるので配列長と一つずれてる
           div.style.opacity = 0.0;
@@ -46,19 +48,21 @@ $(function () {
       }
     });
   });
+  allSectionDivs += divs_section1.length;
+
 
   const divs_section2 = document.querySelectorAll('.section-2');
   allSectionDivs += 1;//section間の余白ようdiv１つ分の高さを追加
-  //allSectionDivs += divs_section2.length;
+
   console.log(divs_section2, divs_section2.length, allSectionDivs);
   divs_section2.forEach((div, i) => {
 
     $(window).scroll(function () {
       var scroll = $(window).scrollTop();
-      if (scroll >= windowHeight * i + allSectionDivs && scroll < windowHeight * (i + allSectionDivs + 1)) {
+      if (scroll >= windowHeight * (i + allSectionDivs) && scroll < windowHeight * (i + allSectionDivs + 1)) {
         console.log("in if" + i);
         div.style.opacity = 1.0;
-      } else if (scroll >= windowHeight * (i + allSectionDivs - 1) && scroll < windowHeight * i + allSectionDivs) {
+      } else if (scroll >= windowHeight * (i + allSectionDivs - 1) && scroll < windowHeight * (i + allSectionDivs)) {
         console.log("in else if");
         div.style.opacity = 0.5;
       } else {
@@ -71,8 +75,31 @@ $(function () {
     });
   });
 
+  var allSectionDivs_2 = allSectionDivs + divs_section2.length;// allSectionDivsをそのまま継ぎ足していくとどうも事故る　なぜだ
 
+  const divs_section3 = document.querySelectorAll('.section-3');
+  allSectionDivs_2 += 1;//section間の余白ようdiv１つ分の高さを追加
 
+  console.log(divs_section3, divs_section3.length, allSectionDivs_2);
+  divs_section3.forEach((div, i) => {
+
+    $(window).scroll(function () {
+      var scroll = $(window).scrollTop();
+      if (scroll >= windowHeight * (i + allSectionDivs_2) && scroll < windowHeight * (i + allSectionDivs_2 + 1)) {
+        console.log("in if" + i);
+        div.style.opacity = 1.0;
+      } else if (scroll >= windowHeight * (i + allSectionDivs_2 - 1) && scroll < windowHeight * (i + allSectionDivs_2)) {
+        console.log("in else if");
+        div.style.opacity = 0.5;
+      } else {
+        console.log("in else");
+        if (i != divs_section3.length - 1) {
+          div.style.opacity = 0.0;
+        }
+
+      }
+    });
+  });
 
 
 });//$(function () のけつ
